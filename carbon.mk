92cb96a25c5ce60b7d0efd9822b579227b264603
Joel Maxuel <joel.maxuel@gmail.com>
2015-12-21 09:52:03 -0400

    Create carbon.mk

---
+++ carbon.mk
@@ -0,0 +1,12 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1200
+
+# Inherit device configuration for shieldtablet.
+$(call inherit-product, device/nvidia/shieldtablet/full_shieldtablet.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_tablet.mk)
+
+PRODUCT_NAME := carbon_shieldtablet
+PRODUCT_DEVICE := shieldtablet
