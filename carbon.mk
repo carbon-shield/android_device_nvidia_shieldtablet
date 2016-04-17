23eff456c07f8483821e7352a01275374e45f0b5
Joel Maxuel <joel.maxuel@gmail.com>
2016-04-16 21:23:06 -0300

    Clean up duplicate and confusing options
    - Screen width and height already defined in device.mk
    - Common Carbon stuff should not be CM's

--- carbon.mk
+++ carbon.mk
@@ -1,11 +1,7 @@
-# Boot animation
-TARGET_SCREEN_HEIGHT := 1920
-TARGET_SCREEN_WIDTH := 1200
-
 # Inherit device configuration for shieldtablet.
 $(call inherit-product, device/nvidia/shieldtablet/full_shieldtablet.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common Carbon stuff.
 $(call inherit-product, vendor/carbon/config/common_tablet.mk)
 
 PRODUCT_NAME := carbon_shieldtablet
